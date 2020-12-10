66ab8d4cda2b09711071d0555f326678d09ebb51
Luis <Luis@Luis-PC>
2020-12-10 09:07:27 -0600

    Modal

---
+++ modal.php
@@ -0,0 +1,51 @@
+ <a data-toggle="modal" data-target="#modalCurso" class="btn text-uppercase btn-outline-danger btn-lg mr-3 mb-3 wow bounceInUp"> Registro cursos</a> 
+ <a data-toggle="modal" data-target="#modalClase" class="btn text-uppercase btn-outline-danger btn-lg mb-3 wow bounceInDown"> Ingreso clases</a> </div>
+  
+
+
+
+<div class="modal fade" id="modalCurso" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+      <!-- Contenido ventana modal -->   
+      
+      
+      <h4 class="modal-title" id="myModalLabel">Registro cursos</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        
+      </div>
+      <div class="col-md-12 footer2 wow bounceInUp" data-wow-delay=".25s" id="contact">
+        <div class="form-box">
+          <h4>CONTACTANOS</h4>
+          <table class="table table-responsive d-table">
+            <tr>
+              <td><input type="text" class="form-control pl-0" placeholder="NOMBRE" /></td>
+              <td><input type="email" class="form-control pl-0" placeholder="EMAIL" /></td>
+            </tr>
+            <tr>
+              <td colspan="2"></td>
+            </tr>
+            <tr>
+              <td colspan="2"><input type="text" class="form-control pl-0" placeholder="DIRECCION" /></td>
+            </tr>
+            <tr>
+              <td colspan="2"></td>
+            </tr>
+            <tr>
+              <td colspan="2"><input type="text" class="form-control pl-0" placeholder="MENSAJE" /></td>
+            </tr>
+            <tr>
+              <td colspan="2"></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="text-center pl-0"><button type="submit" class="btn btn-dark">ENVIAR</button></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>  
