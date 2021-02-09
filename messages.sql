ff946f8ca749d915791eb77faa5e68ac8824a00e
Luis <Luis@Luis-PC>
2021-02-09 16:50:07 -0600

    final

--- messages.sql
+++ messages.sql
@@ -1,3 +1,11 @@
+CREATE TABLE `messages` (
+  `idMessage` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 INSERT INTO `messages` (`idMessage`, `name`, `email`, `message`) VALUES (1, 'luis alfredo rodriguez medellin', 'kartegen@gmail.com', 'asdsadasd');
 INSERT INTO `messages` (`idMessage`, `name`, `email`, `message`) VALUES (2, 'luis alfredo rodriguez medellin', 'kartegen@gmail.com', 'rodriguez medellin');
 INSERT INTO `messages` (`idMessage`, `name`, `email`, `message`) VALUES (3, 'luis alfredo rodriguez medellin', 'kartegen@gmail.com', 'biugoigoi');
